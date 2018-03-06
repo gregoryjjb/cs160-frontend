18c1e6a58dfed0ce1d798a0a06dad66e3b0f829c
Greg Brisebois <gregoryjjb@gmail.com>
2018-03-05 23:20:53 -0800

    Added loading overlay to block videos

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -3,14 +3,29 @@ import { List } from 'rmwc/List';
 import { LinearProgress } from 'rmwc/LinearProgress';
 import VideoListItem from 'components/VideoListItem';
 
+
+const divStyle = {
+	position: 'relative'
+}
+
+const loadingOverlayStyle = {
+	position: 'absolute',
+	top: 0,
+	bottom: 0,
+	left: 0,
+	right: 0,
+	backgroundColor: 'rgba(0,0,0,0.25)'
+}
+
 const VideoList = ({videos, loading}) => (
-	<div>
+	<div style={divStyle} >
 		<LinearProgress determinate={false} style={{ display: (loading ? 'block' : 'none') }} />
 		<List>
 			{videos.map(video => (
 				<VideoListItem video={video} />
 			))}
 		</List>
+		<div style={{...loadingOverlayStyle, display: (loading ? 'block' : 'none') }}></div>
 	</div>
 )
 
