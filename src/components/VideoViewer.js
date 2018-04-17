eaa7f4fe93941c87a63d391bf2ce2dbd12501956
Greg Brisebois <gregoryjjb@gmail.com>
2018-04-16 23:55:02 -0700

    Added extra information to video viewer

--- src/components/VideoViewer.js
+++ src/components/VideoViewer.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import { Typography } from 'rmwc/Typography';
+import { css } from 'glamor';
 
 const blankVideo = {
 	name: 'Select a video',
-	path: ''
+	path: '',
+	date: '',
 }
 
 const VideoViewer = ({video = blankVideo, style}) => (
@@ -10,7 +13,10 @@ const VideoViewer = ({video = blankVideo, style}) => (
 		<video controls style={{width: '100%'}} key={video.path} >
 			<source src={video.path} />
 		</video>
-		<p>{video.name}</p>
+		<div style={{width: '100%', padding: '0 1em', boxSizing: 'border-box'}}>
+			<p><Typography use="headline">{video.name}</Typography></p>
+			<p style={{color: '#888'}} >Uploaded {new Date(Date.parse(video.createdAt)).toLocaleDateString()}</p>
+		</div>
 	</div>
 )
 
